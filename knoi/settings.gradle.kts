b6954843ab4383aa28f18be8124a851356a0d783
qizhengchen <qizhengchen@tencent.com>
2025-05-27 11:34:07 +0800

    publishing config

--- knoi/settings.gradle.kts
+++ knoi/settings.gradle.kts
@@ -3,9 +3,6 @@ pluginManagement {
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
-//        maven("https://mirrors.tencent.com/repository/maven/tencentvideo")
-//        maven("https://mirrors.tencent.com/repository/maven/tmm-snapshot")
-//        mavenLocal()
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -14,12 +11,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-//        mavenLocal()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
-//        maven("https://mirrors.tencent.com/repository/maven/tencentvideo")
-//        maven("https://mirrors.tencent.com/repository/maven/tmm-snapshot")
         google()
         mavenCentral()
     }
